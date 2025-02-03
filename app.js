18683b9f46caa4658a2e58566856f1e0fe3545bf
Squizard <184549450+BradyLeg@users.noreply.github.com>
2025-02-02 23:37:08 -0800

    created and completed project files

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+
+const form = [];
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+app.post('/submit', (req, res) => {
+    console.log(req.body);
+    form.push(req.body);
+    res.send(`<h1>Thank you for filling out the form ${req.body.fname}!</h1>`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
